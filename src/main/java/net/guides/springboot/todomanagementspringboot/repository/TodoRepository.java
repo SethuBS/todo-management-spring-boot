83f5fa038f4af740267e2b959a8056176221d0dd
Sethu Budaza <sethu.budaza@payu.com>
2022-10-05 12:00:16 +0200

    backend and service layer

--- src/main/java/net/guides/springboot/todomanagementspringboot/repository/TodoRepository.java
+++ src/main/java/net/guides/springboot/todomanagementspringboot/repository/TodoRepository.java
@@ -1,2 +1,10 @@
-package net.guides.springboot.todomanagementspringboot.repository;public interface TodoRepository {
+package net.guides.springboot.todomanagementspringboot.repository;
+
+import net.guides.springboot.todomanagementspringboot.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+        List<Todo> findByUserName(String user);
 }
--- src/main/java/net/guides/springboot/todomanagementspringboot/service/ITodoService.java
+++ src/main/java/net/guides/springboot/todomanagementspringboot/service/ITodoService.java
@@ -1,2 +1,21 @@
-package net.guides.springboot.todomanagementspringboot.service;public interface ITodoService {
+package net.guides.springboot.todomanagementspringboot.service;
+
+import net.guides.springboot.todomanagementspringboot.model.Todo;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public interface ITodoService {
+    List<Todo> getTodosByUser(String user);
+
+    Optional< Todo > getTodoById(long id);
+
+    void updateTodo(Todo todo);
+
+    void addTodo(String name, String desc, Date targetDate, boolean isDone);
+
+    void deleteTodo(long id);
+
+    void saveTodo(Todo todo);
 }
